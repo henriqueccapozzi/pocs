513f53163c4b4711f297acd492a09301a645f846
Henrique C. Capozzi <henriqueccapozzi@gmail.com>
2021-02-21 17:21:58 -0300

    Install selenium

--- docker-selenium/app.py
+++ docker-selenium/app.py
@@ -19,14 +19,34 @@ def set_chrome_options() -> None:
     return chrome_options
 
 
+def simple_login_test(driver) -> None:
+    URL = "https://the-faster.herokuapp.com/login"
+    USER = "candidato1@gmail.com"
+    PASS = "2SZ&18U1KiY"
+
+    driver.get(URL)
+    driver.set_window_size(800, 600)
+
+    email_element = driver.find_element_by_name("email")
+    password_element = driver.find_element_by_name("password")
+
+    email_element.send_keys(USER)
+    # email_element.send_keys(Keys.ENTER)
+
+    password_element.send_keys(PASS)
+    password_element.send_keys(Keys.ENTER)
+    time.sleep(5)
+    driver.find_elements_by_link_text("Minha Conta")
+
+
 def get_element_text(driver: webdriver.Chrome) -> None:
     URL = "https://the-faster.herokuapp.com/login"
     driver.get(URL)
+    driver.save_screenshot("full.png")
     elements = driver.find_elements_by_tag_name("input")
     for element in elements:
-        print(element.get_property("name"))
         print("*" * 60)
-        # print(dir(element))
+        print(element.get_property("name"))
 
 
 if __name__ == "__main__":
